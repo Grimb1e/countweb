74bafd5bec37bece1ab7a060863cccbfab7c2a88
unknown <batu73580@gmail.com>
2023-05-07 01:13:08 +0400

    init

--- script.js
+++ script.js
@@ -16,12 +16,19 @@ document.querySelector(".btn-increment").addEventListener("click", () => {
 });
 
 document.querySelector(".btn-decrement").addEventListener("click", () => {
-  if (score <= 0) {
-    return;
-  }
+  document.querySelector(".btn-decrement").addEventListener("click", () => {
+    if (score <= 0 || isButtonDisabled) {
+      return;
+    }
 
-  score--;
-  document.querySelector("#score").textContent = score;
+    isButtonDisabled = true;
+    setTimeout(() => {
+      isButtonDisabled = false;
+    }, 900);
+
+    score--;
+    document.querySelector("#score").textContent = score;
+  });
 });
 
 document.querySelector(".btn-reset").addEventListener("click", () => {
