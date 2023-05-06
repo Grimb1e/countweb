0e45f696133155712d2f0ecd51abeb26384ab19b
unknown <batu73580@gmail.com>
2023-05-07 00:08:10 +0400

    init

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 let score = 0;
+let isButtonDisabled = false;
 
 document.querySelector(".btn-increment").addEventListener("click", () => {
+  if (isButtonDisabled) {
+    return; 
+  }
+
+  isButtonDisabled = true;
+  setTimeout(() => {  
+    isButtonDisabled = false;
+  }, 900);
+  
   score++;
   document.querySelector("#score").textContent = score;
 });
@@ -9,11 +19,12 @@ document.querySelector(".btn-decrement").addEventListener("click", () => {
   if (score <= 0) {
     return;
   }
+
   score--;
-    document.querySelector("#score").textContent = score;
+  document.querySelector("#score").textContent = score;
 });
 
-document.querySelector(".btn-reset").addEventListener('click', () => {
+document.querySelector(".btn-reset").addEventListener("click", () => {
   score = 0;
-  document.querySelector('#score').textContent = score;
+  document.querySelector("#score").textContent = score;
 });
